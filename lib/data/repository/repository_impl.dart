8a01f1b5b69099547155bbbeb14bdc083b229622
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2022-01-10 17:24:35 -0300

    Data Layer - Applying ErrorHandler on Repository Impl

---
+++ lib/data/repository/repository_impl.dart
@@ -0,0 +1,41 @@
+import 'package:complete_advanced_flutter/data/data_source/remote_data_source.dart';
+import 'package:complete_advanced_flutter/data/network/error_handler.dart';
+import 'package:complete_advanced_flutter/data/network/failure.dart';
+import 'package:complete_advanced_flutter/data/network/network_info.dart';
+import 'package:complete_advanced_flutter/data/request/request.dart';
+import 'package:complete_advanced_flutter/domain/model.dart';
+import 'package:complete_advanced_flutter/domain/repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:complete_advanced_flutter/data/mapper/mapper.dart';
+
+class RepositoryImpl extends Repository {
+  RemoteDataSource _remoteDataSource;
+  NetworkInfo _networkInfo;
+
+  RepositoryImpl(this._remoteDataSource, this._networkInfo);
+
+  @override
+  Future<Either<Failure, Authentication>> login(
+      LoginRequest loginRequest) async {
+    if (await _networkInfo.isConnected) {
+      try {
+        // Its safe to call the API
+        final response = await _remoteDataSource.login(loginRequest);
+        if (response.status == 0) // Success
+        {
+          // return data (success)
+          return Right(response.toDomain());
+        } else {
+          // return Biz logic error
+          return Left(Failure(response.status ?? ApiInternalStatus.FAILURE,
+              response.message ?? ResponseMessage.DEFAULT));
+        }
+      } catch (error) {
+        return (Left(ErrorHandler.handle(error).failure));
+      }
+    } else {
+      // Return connection error
+      return Left(DataSource.NO_INTERNET_CONNECTION.getFailure());
+    }
+  }
+}
