64bd70480f02d508a88215a1304d22c118e04b49
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2022-01-13 16:45:33 -0300

    Lecture 64 - Presentation Layer - Adding Language Manager

--- lib/app/constants.dart
+++ lib/app/constants.dart
@@ -1,3 +1,4 @@
 class Constants{
   static const String baseUrl = "http://boasvendas.mocklab.io";
+  static const String token = "get API token";
 }
---
+++ lib/data/network/dio_factory.dart
@@ -0,0 +1,43 @@
+import 'package:complete_advanced_flutter/app/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+const String APPLICATION_JSON = "application/json";
+const String CONTENT_TYPE = "content-type";
+const String ACCEPT = "accept";
+const String AUTHORIZATION = "authorization";
+const String DEFAULT_LANGUAGE = "language";
+
+class DioFactory {
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    int _timeOut = 60 * 1000; // 1 minuto
+
+    Map<String, String> headers = {
+      CONTENT_TYPE: APPLICATION_JSON,
+      ACCEPT: APPLICATION_JSON,
+      AUTHORIZATION: Constants.token,
+      DEFAULT_LANGUAGE: "en", // TODO: Get lang from app prefs
+    };
+    dio.options = BaseOptions(
+      baseUrl: Constants.baseUrl,
+      connectTimeout: _timeOut,
+      receiveTimeout: _timeOut,
+      headers: headers,
+    );
+
+    if (kReleaseMode) {
+      print('Release mode no logs');
+    } else {
+      dio.interceptors.add(PrettyDioLogger(
+        requestHeader: true,
+        requestBody: true,
+        responseHeader: true,
+      ));
+    }
+
+    return dio;
+  }
+}
