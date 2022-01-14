427ac57374eabd85699034e5f2c6eeb7f50cecff
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2022-01-14 19:17:25 -0300

    Lição 76

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,3 +1,4 @@
+import 'package:complete_advanced_flutter/presentation/login/login_viewmodel.dart';
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
@@ -8,8 +9,33 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+  LoginViewModel _viewModel = LoginViewModel(null);
+
+  TextEditingController _userNameController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  _bind() {
+    _viewModel.start();
+    _userNameController
+        .addListener(() => _viewModel.setUserName(_userNameController.text));
+    _passwordController
+        .addListener(() => _viewModel.setPassword(_passwordController.text));
+  }
+
+  @override
+  void initState() {
+    _bind();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
+  }
+
+  @override
+  void dispose() {
+    _viewModel.dispose();
+    super.dispose();
   }
 }
